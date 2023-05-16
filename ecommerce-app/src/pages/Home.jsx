8b25483c90b678ea754b4a0eac0e6991abcb90f7
Manuel Murillo <mmpeira@gmail.com>
2023-05-16 16:01:40 +0200

    landing terminada

---
+++ ecommerce-app/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../componets/Navbar'
+import Announcement from '../componets/Announcement'
+import Slider from '../componets/Slider'
+
+const Home = () => {
+  return (
+    <div>
+      <Announcement/>
+      <Navbar/>
+      <Slider/>
+      
+
+    </div>
+  )
+}
+
+export default Home
